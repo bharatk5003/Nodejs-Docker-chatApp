c6d9700e27695c467bdbbaac2c1d5b8283f2a163
Bharat Kumar <bhakumar19@publicisgroupe.net>
2022-06-02 02:04:21 +0530

    deployed chat app

---
+++ dockerfile
@@ -0,0 +1,8 @@
+FROM node:latest
+WORKDIR /app
+COPY package.json .
+RUN npm install -g nodemon
+RUN npm install socket.io
+COPY . .
+
+CMD ["npm","start"]
---
+++ index.js
@@ -0,0 +1,50 @@
+const path=require('path');
+const http=require('http');
+const express=require('express');
+const socketIO=require('socket.io');
+
+
+const publicPath=path.join(__dirname,'/public');
+console.log(publicPath);
+const port=process.env.PORT || 3000;
+const app=express();
+const server=http.createServer(app);
+const io=socketIO(server);
+
+
+app.use(express.static(publicPath));
+
+
+console.log(path.join(__dirname+'/public/demo.html'));
+app.get('/',(req,res)=>{
+     
+    res.sendFile(path.join(__dirname+'/public/demo.html'));
+});
+
+
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("new user joined the chat", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
